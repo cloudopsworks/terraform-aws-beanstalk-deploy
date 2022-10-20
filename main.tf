aea36e9726265fecd5065304c8d51ca86859cd5a
Cristian Beraha <cristian@cloudops.works>
2022-10-20 12:09:17 -0300

    map conversion

--- main.tf
+++ main.tf
@@ -46,10 +46,10 @@ resource "aws_elastic_beanstalk_environment" "beanstalk_environment" {
   dynamic "setting" {
     for_each = local.eb_settings
     content {
-      name      = setting.value["name"]
-      namespace = setting.value["namespace"]
-      resource  = setting.value["resource"]
-      value     = setting.value["value"]
+      name      = setting.value.name
+      namespace = setting.value.namespace
+      resource  = setting.value.resource
+      value     = setting.value.value
     }
   }
 }
