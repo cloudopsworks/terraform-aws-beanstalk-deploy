a9b301a1ba775382326ac2adca03fdb3638a0029
Cristian Beraha <cristian@cloudops.works>
2023-06-13 16:40:17 -0300

    Added useful outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,20 @@
 output "environment_cname" {
   value = aws_elastic_beanstalk_environment.beanstalk_environment.cname
 }
+
+data "aws_alb" "beanstalk_alb" {
+  arn = aws_elastic_beanstalk_environment.beanstalk_environment.load_balancers[0]
+}
+
+output "load_balancer_address" {
+  value = data.aws_alb.beanstalk_alb.dns_name
+}
+
+output "load_balancer_zone_id" {
+  value = data.aws_alb.beanstalk_alb.zone_id
+}
+
+output "environment_id" {
+  value = aws_elastic_beanstalk_environment.beanstalk_environment.id
+}
+
