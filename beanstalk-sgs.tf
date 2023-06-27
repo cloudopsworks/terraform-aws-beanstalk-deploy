87820b04ba5ea7b91abc236efeb35ba322023844
Cristian Beraha <cristian@cloudops.works>
2023-06-27 13:30:02 -0300

    FIX on SG count

--- beanstalk-sgs.tf
+++ beanstalk-sgs.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "instance_sg" {
 # SG rules for load balancer
 # Allow all outbound traffic from the load balancer
 resource "aws_vpc_security_group_egress_rule" "egress_rule_lb" {
-  count = length(var.beanstalk_target_sg) > 0 ? 1 : 0
+  count = length(var.beanstalk_lb_sg) > 0 ? 1 : 0
 
   security_group_id = aws_security_group.lb_sg[0].id
   from_port         = 0
@@ -126,7 +126,7 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_rule_lb_sg" {
 # Instance SG rules
 # Allow all outbound traffic Target group
 resource "aws_vpc_security_group_egress_rule" "egress_rule_tgt" {
-  count = var.beanstalk_target_sg != null ? 1 : 0
+  count = length(var.beanstalk_target_sg) > 0 ? 1 : 0
 
   security_group_id = aws_security_group.instance_sg[0].id
   from_port         = 0
