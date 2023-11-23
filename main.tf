52a78fb415cfe6c5ef22a707f08e8fed6b5f46e8
Cristian Beraha <cristian@cloudops.works>
2023-11-23 13:35:51 -0300

    fix: incorrect count

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_elastic_beanstalk_environment" "beanstalk_environment" {
   })
 
   lifecycle {
-    create_before_destroy = true
+    #create_before_destroy = true
     replace_triggered_by = [
       null_resource.shared_lb_rules
     ]
