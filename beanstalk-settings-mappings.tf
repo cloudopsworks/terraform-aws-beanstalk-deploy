bdf442c88c6661f4363f44ab2fcd2e29d4fffef7
Cristian Beraha <cristian@cloudops.works>
2023-11-23 11:25:49 -0300

    fix: host headers concatenation.

--- beanstalk-settings-mappings.tf
+++ beanstalk-settings-mappings.tf
@@ -258,7 +258,7 @@ locals {
         name      = "HostHeaders"
         namespace = "aws:elbv2:listenerrule:${r.name}"
         resource  = ""
-        value     = r.host
+        value     = load_balancer_alias(",", [r.host , "${var.load_balancer_alias}.${var.region}.elasticbeanstalk.com"])
       },
       {
         name      = "PathPatterns"
